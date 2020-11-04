e8af23864aeaa33762d1932b12d51e794b628207
Mohammed Aman Khan <mohdamankhan290@gmail.com>
2020-11-04 10:32:37 +0530

    first commit

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 import pandas as pd
-import pickle
 import random
 import json
 import tensorflow as tf
